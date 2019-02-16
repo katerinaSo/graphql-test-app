5177b4146169b74e9e25e9f8eb057fef35f3087e
KaterinaSo <katya.solstad@gmail.com>
2019-02-16 08:49:13 -0500

    first commit

---
+++ schema.js
@@ -0,0 +1,35 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const _ = require("lodash");
+
+const users = [
+  { id: "20", firstName: "John", age: 23 },
+  { id: "30", firstName: "Tracy", age: 25 }
+];
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const expressGraphQl = require("express-graphql");
+const schema = require("./schema");
+
+const app = express();
+app.use("/graphql", expressGraphQl({ schema, graphiql: true }));
+app.listen(4000, () => {
+  console.log("listening");
+});
